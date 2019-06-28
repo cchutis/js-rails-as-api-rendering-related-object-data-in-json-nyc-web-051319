f519cd0b05d78546891ecea1973a1c895e9839f7
Constantine Chutis <constantinec84@gmail.com>
2019-06-28 08:20:18 -0400

    Finished

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,12 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: sightings, include: [:bird, :location]
   end
+
+  def show
+    sighting = Sighting.find_by(id: params[:id])
+    render json: sighting, include: [:bird, :location]
+  end
+
 end
